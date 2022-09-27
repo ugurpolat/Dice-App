8c262a099f832989fcaa2ae98c040a6dea1738d0
Ugur Polat <ugurpolat113@gmail.com>
2022-09-28 01:08:39 +0300

    Add roll logic.

--- Dice App/Dice App/ViewController.swift
+++ Dice App/Dice App/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var dices: [UIImageView]!
+    @IBOutlet weak var rollButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rollButton.layer.cornerRadius = 20;
     }
 
-
+    @IBAction func clickRoll(_ sender: Any) {
+        dices[0].image = UIImage(named: "dice-\(randomNumber())");
+        dices[1].image = UIImage(named: "dice-\(randomNumber())");
+    }
+    
+    func randomNumber() -> (Int) {
+        let diceNumber:Int = Int.random(in: 1..<7)
+        
+        return diceNumber;
+    }
 }
 
